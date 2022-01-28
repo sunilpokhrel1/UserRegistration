dd8243f22d7bb9c3adf95d98c830ef27000af8ff
sunilpokhrel1 <sunil073bex@ioepc.edu.np>
2022-01-29 00:56:20 +0545

    Add project files.

---
+++ UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit( int id = 0)
+        {
+            Table user = new Table();
+
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Table user)
+        {
+            using (DBModels db = new DBModels())
+            {
+                if (db.Tables.Any(x => x.UserName == user.UserName))
+                {
+                    ViewBag.DuplicateMessage = "User Name Already Exists.";
+                    return View("AddOrEdit", user);
+
+                        }
+                else
+                {
+                    db.Tables.Add(user);
+                    db.SaveChanges();
+                }
+                
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+                return View("AddOrEdit",new Table());
+
+        }
+    }
+}
